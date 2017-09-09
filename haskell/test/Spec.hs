35ba6fef5d45800c5db203370229c32f25ff9281
Roman Kivalin <roman@kivalin.me>
2017-09-09 22:12:59 +0400

    test(haskell): add test for problem1

--- haskell/test/Spec.hs
+++ haskell/test/Spec.hs
@@ -1,2 +1,11 @@
+{-# OPTIONS_GHC -F -pgmF htfpp #-}
+
+import Test.Framework
+import Euler
+
+test_problem1 = do
+    assertEqual 23 $ problem1 10 [3, 5]
+    assertEqual 233168 $ problem1 1000 [3, 5]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = htfMain htf_thisModulesTests
