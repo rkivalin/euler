e03507438aa8d3eba2b171da0accd19595a00677
Roman Kivalin <roman@kivalin.me>
2017-09-09 23:44:49 +0400

    feat(haskell): solution to the problem3

--- haskell/src/Euler.hs
+++ haskell/src/Euler.hs
@@ -1,20 +1,40 @@
 module Euler
 ( problem1
 , problem2
+, problem3
 ) where
 
 import Data.List (foldl')
+
+isqrt :: (Integral a) => a -> a
+isqrt = floor . sqrt . fromIntegral
 
 x `multipleOf` y = x `mod` y == 0
 
 x `multipleOfAny` [] = False
 x `multipleOfAny` (y:ys) = x `multipleOf` y || x `multipleOfAny` ys
 
+fibonacciSeq :: (Integral a) => [a]
 fibonacciSeq = seq (0, 1) where
     seq (a, b) = a : seq (b, a + b)
+
+isPrime :: (Integral a) => a -> Bool
+isPrime x = not $ x `multipleOfAny` primes where
+    primes = takeWhile (<= isqrt x) primeSeq
+
+primeSeq :: (Integral a) => [a]
+primeSeq = 2 : filter isPrime [3,5..]
+
+primeFactors :: (Integral a) => a -> [a]
+primeFactors x = factors x primeSeq where
+    factors 1 _ = []
+    factors x (f:fs) = if x `multipleOf` f then f:(factors (x `div` f) (f:fs)) else factors x fs
 
 problem1 :: Integer -> [Integer] -> Integer
 problem1 n divisors = foldl' (+) 0 $ filter (`multipleOfAny` divisors) [1..(n-1)]
 
 problem2 :: Integer -> Integer
 problem2 n = foldl' (+) 0 $ filter even $ takeWhile (<n) fibonacciSeq
+
+problem3 :: Integer -> Integer
+problem3 = last . primeFactors
--- haskell/test/Spec.hs
+++ haskell/test/Spec.hs
@@ -10,5 +10,9 @@ test_problem1 = do
 test_problem2 = do
     assertEqual 4613732 $ problem2 (4 * 10^6)
 
+test_problem3 = do
+    assertEqual 29 $ problem3 13195
+    assertEqual 6857 $ problem3 600851475143
+
 main :: IO ()
 main = htfMain htf_thisModulesTests
